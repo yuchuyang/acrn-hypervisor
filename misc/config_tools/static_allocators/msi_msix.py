740ac1ae461b7c6f12412b7560a73268fb8cdef1
Yang,Yu-chu <yu-chu.yang@intel.com>
2021-06-23 14:49:35 -0700

    refine static allocator
    Signed-off-by: Yang,Yu-chu <yu-chu.yang@intel.com>

--- misc/config_tools/static_allocators/msi_msix.py
+++ misc/config_tools/static_allocators/msi_msix.py
@@ -13,4 +13,10 @@ def fn(board_etree, scenario_etree, allocation_etree):
     msi_count = board_etree.xpath("//capability[@id = 'MSI']/count/text()")
     msix_table_size = board_etree.xpath("//capability[@id = 'MSI-X']/table_size/text()")
     table = [int(i) for i in msi_count + msix_table_size]
-    common.append_node("/acrn-config/hv/MAX_MSIX_TABLE_NUM", str(max(table)), allocation_etree)
+    max_msix_table_num = 64
+    try:
+        max_msix_table_num = max(table)
+    except:
+        common.print_yel("Cannot find the MSI counts and MSI-X table_siz from board xml, MAX_MSIX_TABLE_NUM sets to default value 64.", warn=True)
+        pass
+    common.append_node("/acrn-config/hv/MAX_MSIX_TABLE_NUM", str(max_msix_table_num), allocation_etree)
